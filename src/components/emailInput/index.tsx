fc8d35ea7b0d6cf39b95c6625012acd842fd3f39
Andrew Kuktenko <andriy.kuktenko@gmail.com>
2023-07-19 00:58:40 +0300

    fix password & email inputs and add ts

--- src/components/emailInput/index.tsx
+++ src/components/emailInput/index.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react';
 import { Container, Input, Icon } from './styles';
+import { EmailInputProps } from 'ts/types/email.input.props';
 
-const EmailInput: FC<any> = ({ onChange, value, register, required, ...props }) => {
+const EmailInput: FC<EmailInputProps> = ({ register, required, ...props }) => {
   return (
       <Container>
           <Icon/>
-          <Input autoComplete='off'  {...register('email', { required, pattern: /^\S+@\S+$/i })} {...props} placeholder="Email" onChange={onChange} value={value} name="email" />
+          <Input autoComplete='off'  {...register('email', { required, pattern: /^\S+@\S+$/i })} {...props} placeholder="Email" name="email" />
       </Container>
   )
 }
